8be81cb1338df4e0ac40a329df8bcd3500219d99
griff182uk <griff182uk@yahoo.co.uk>
2023-11-25 14:29:39 +0000

    done module 1

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Random random = new Random();
+// int coinFlip = random.Next(0,2);
+// string coinSide = coinFlip == 1 ? "Heads" : "Tails";
+// Console.WriteLine(coinSide);
+
+string permission = "jim";
+int level = 56;
+string message = "You do not have sufficient privileges.";
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        message = "Welcome, Super Admin user.";
+    }
+    else
+    {
+        message = "Welcome, Admin user.";
+    }
+}
+if (permission.Contains("Manager"))
+{
+    if (level > 20)
+    {
+        message = "Welcome, Super Manager user.";
+    }
+
+}
+
+Console.WriteLine(message);
+
