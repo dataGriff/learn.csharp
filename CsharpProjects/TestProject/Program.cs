19c4920ad04fee6915bdbcd8499488c94911e924
griff182uk <griff182uk@yahoo.co.uk>
2023-11-27 19:25:39 +0000

    Refactor code and remove commented out sections

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -391,43 +391,250 @@ do
 
 // Console.WriteLine(largerValue);
 
-Random dice = new Random();
+// Random dice = new Random();
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
 
-int total = roll1 + roll2 + roll3;
+// int total = roll1 + roll2 + roll3;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     Console.WriteLine("You rolled doubles! +2 bonus to total!");
+//     total += 2;
+// }
+
+// if ((roll1 == roll2) && (roll2 == roll3)) 
+// {
+//     Console.WriteLine("You rolled triples! +6 bonus to total!");
+//     total += 6;
+// }
+
+// if (total >= 15)
+// {
+//     Console.WriteLine("You win!");
+// }
+
+// if (total < 15)
+// {
+//     Console.WriteLine("Sorry, you lose.");
+// }
+
+// string message = "The quick brown fox jumps over the lazy dog.";
+// bool result = message.Contains("dog");
+// Console.WriteLine(result);
+
+// if (message.Contains("fox"))
+// {
+//     Console.WriteLine("What does the fox say?");
+// }
+
+// remove stacking bonus
+
+// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// int total = roll1 + roll2 + roll3;
+
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     if ((roll1 == roll2) && (roll2 == roll3))
+//     {
+//         Console.WriteLine("You rolled triples!  +6 bonus to total!");
+//         total += 6;
+//     }
+//     else
+//     {
+//         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+//         total += 2;
+//     }
+// }
+
+// if (total >= 16)
+// {
+//     Console.WriteLine("You win a new car!");
+// }
+// else if (total >= 10)
+// {
+//     Console.WriteLine("You win a new laptop!");
+// }
+// else if (total == 7)
+// {
+//     Console.WriteLine("You win a trip for two!");
+// }
+// else
+// {
+//     Console.WriteLine("You win a kitten!");
+// }
+
+// challenge
+
+// Random random = new Random();
+// int daysUntilExpiration = random.Next(12);
+// int discountPercentage = 0;
+
+// if(daysUntilExpiration < 10)
+// {
+//     if(daysUntilExpiration ==0)
+//     {
+//         Console.WriteLine("Your subscription has expired.");
+//     }
+//     else if (daysUntilExpiration ==1)
+//     {
+//         Console.WriteLine("Your subscription expires in a day!");
+//         Console.WriteLine("Renew now and save 20%!");
+//         discountPercentage = 20;
+//     }
+//     else if (daysUntilExpiration <= 5)
+//     {
+//         Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+//         Console.WriteLine("Renew now and save 10%!");
+//         discountPercentage = 10;
+//     }
+//     else {
+//         Console.WriteLine("Your subscription will expire soon. Renew now!"); 
+//     }
+// }
+
+// string[] fraudulentOrderIDs = new string[3];
+
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+// fraudulentOrderIDs[3] = "D000";
+
+// string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+// fraudulentOrderIDs[0] = "F000";
+
+// Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+// Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+// string[] names = { "Rowena", "Robin", "Bao" };
+// foreach (string name in names)
+// {
+//     Console.WriteLine(name);
+// }
+
+// int[] inventory = { 200, 450, 700, 175, 250 };
+// int sum = 0;
+// int bin = 0;
+// foreach (int items in inventory)
+// {
+//     sum += items;
+//     bin++;
+//     Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+// }
+
+// Console.WriteLine($"We have {sum} items in inventory.");
+
+// challenge
+
+// string [] orders = {"B123","C234","A345","C15","B177","G3003","C35","B179" };
+
+// foreach (string order in orders)
+// {
+//     if (order.StartsWith("B"))
+//     {
+//          Console.WriteLine(order);
+//     }
+// }
+
+// good variable names
+
+char userOption;
+
+int gameScore;
+
+float particlesPerMillion;
+
+bool processedCustomer;
+
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
 {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
 }
 
-if ((roll1 == roll2) && (roll2 == roll3)) 
+foreach (var orderID in orderIDs)
 {
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
+    Console.WriteLine(orderID);
 }
 
-if (total >= 15)
-{
-    Console.WriteLine("You win!");
-}
+// good whitespace
 
-if (total < 15)
-{
-    Console.WriteLine("Sorry, you lose.");
-}
+// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// int total = roll1 + roll2 + roll3;
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
+// {
+//     if ((roll1 == roll2) && (roll2 == roll3)) 
+//     {
+//         Console.WriteLine("You rolled triples!  +6 bonus to total!");
+//         total += 6; 
+//     } 
+//     else 
+//     {
+//         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+//         total += 2;
+//     }
+// }
+
+// challenge
+
+/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
 
 string message = "The quick brown fox jumps over the lazy dog.";
-bool result = message.Contains("dog");
-Console.WriteLine(result);
 
-if (message.Contains("fox"))
-{
-    Console.WriteLine("What does the fox say?");
+char[] characterMessage = message.ToCharArray();
+Array.Reverse(characterMessage);
+
+int letterCount = 0;
+
+foreach (char letter in characterMessage) 
+{ 
+    if (letter == 'o') 
+    { 
+        letterCount++; 
+    } 
 }
+
+string newMessage = new String(characterMessage);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
