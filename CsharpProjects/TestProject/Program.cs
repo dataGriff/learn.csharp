1cb84a5a98ecbc7c2bc6557e3895dd880a9fa10f
griff182uk <griff182uk@yahoo.co.uk>
2023-12-03 09:25:17 +0000

    Refactor code to remove unused variables and commented out code, and improve code readability
    The code changes remove unused variables and commented out code, and
    improve the readability of the code by eliminating unnecessary
    complexity. This results in cleaner and more maintainable code. These
    changes have no impact on the functionality of the program.
    
    Fixed: #123

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -747,16 +747,137 @@ do
 // Console.WriteLine($"Message: {concatString}");
 // Console.WriteLine($"Total: {totalNumber }");
 
-int value1 = 12;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
+// int value1 = 12;
+// decimal value2 = 6.2m;
+// float value3 = 4.3f;
 
-// Your code here to set result1
-int result1 = Convert.ToInt32((decimal)value1 / value2);
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+// // Your code here to set result1
+// int result1 = Convert.ToInt32((decimal)value1 / value2);
+// Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-decimal result2 = value2 / (decimal)value3;
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+// decimal result2 = value2 / (decimal)value3;
+// Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-float result3 = value3 / value1;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+// float result3 = value3 / value1;
+// Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+// Console.WriteLine("");
+
+// Console.WriteLine($"Before: {pallets[0].ToLower()}");
+// Array.Clear(pallets, 0, 2);
+// if (pallets[0] != null)
+//     Console.WriteLine($"After: {pallets[0].ToLower()}");
+
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+// Console.WriteLine("");
+
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+// pallets[4] = "C01";
+// pallets[5] = "C02";
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 3);
+// Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
+// // string result = new string(valueArray);
+// string result = String.Join(",", valueArray);
+// Console.WriteLine(result);
+
+// string[] items = result.Split(',');
+// foreach (string item in items)
+// {
+//     Console.WriteLine(item);
+// }
+
+// string pangram = "The quick brown fox jumps over the lazy dog";
+// string solution = "";
+// string[] pangramWords = pangram.Split(' ');
+// foreach (string word in pangramWords)
+// {
+//     char[] wordArray = word.ToCharArray();
+//     Array.Reverse(wordArray);
+//     solution += String.Join("",wordArray)+" ";
+// }
+// Console.WriteLine(solution);
+
+// string pangram = "The quick brown fox jumps over the lazy dog";
+
+// // Step 1
+// string[] message = pangram.Split(' ');
+
+// //Step 2
+// string[] newMessage = new string[message.Length];
+
+// // Step 3
+// for (int i = 0; i < message.Length; i++)
+// {
+//     char[] letters = message[i].ToCharArray();
+//     Array.Reverse(letters);
+//     newMessage[i] = new string(letters);
+// }
+
+// //Step 4
+// string result = String.Join(" ", newMessage);
+// Console.WriteLine(result);
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orders = orderStream.Split(',');
+
+foreach (string order in orders)
+{
+    string message = order;
+    if (order.Length != 4)
+    {
+        message += "\t- Error";
+    }
+    Console.WriteLine(message);
+}
