822eae50f807ecc3dd33263ed8b04985e2069240
griff182uk <griff182uk@yahoo.co.uk>
2023-12-03 08:58:23 +0000

    done cast and convert

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -705,8 +705,58 @@ do
 // int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
 // Console.WriteLine(result);
 
-int value = (int)1.5m; // casting truncates
-Console.WriteLine(value);
+// int value = (int)1.5m; // casting truncates
+// Console.WriteLine(value);
 
-int value2 = Convert.ToInt32(1.5m); // converting rounds up
-Console.WriteLine(value2);
+// int value2 = Convert.ToInt32(1.5m); // converting rounds up
+// Console.WriteLine(value2);
+
+//string value = "102";
+// string value = "bad";
+// int result = 0;
+// if (int.TryParse(value, out result))
+// {
+//     Console.WriteLine($"Measurement: {result}");
+// }
+// else
+// {
+//     Console.WriteLine("Unable to report the measurement.");
+// }
+
+// if (result > 0)
+//     Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+// decimal totalNumber = 0m;
+// string concatString = "";
+
+// foreach (string value in values)
+// {
+//   decimal number;
+  
+//    if (decimal.TryParse(value, out number))
+//    {
+//       totalNumber += number;
+//    }
+//    else
+//    {
+//       concatString += value;
+//    }
+// }
+
+// Console.WriteLine($"Message: {concatString}");
+// Console.WriteLine($"Total: {totalNumber }");
+
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// Your code here to set result1
+int result1 = Convert.ToInt32((decimal)value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
