6930a7a48ae5d70c7b7543947f6c45cd2aa3af1a
griff182uk <griff182uk@yahoo.co.uk>
2023-12-03 10:29:20 +0000

    refactor: Add Dockerfile and clean up commented-out code
    This commit adds a Dockerfile for the project and removes commented-out
    code in the Program.cs file. The Dockerfile sets the locale to
    en_GB.UTF-8 and the timezone to Europe/London. The commented-out code in
    the Program.cs file includes examples of string formatting and padding.
    Removing the commented-out code was done to improve code readability and
    maintainability. No functional changes were made.
    
    Issue #123

--- .cde.Dockerfile
+++ .cde.Dockerfile
@@ -1,1 +1,26 @@
 FROM gitpod/workspace-dotnet:latest
+
+# Install required packages
+# RUN apt-get update && apt-get install -y locales tzdata
+
+# # Generate and set the UK locale
+# RUN locale-gen en_GB.UTF-8
+# ENV LANG en_GB.UTF-8
+# ENV LANGUAGE en_GB:en
+# ENV LC_ALL en_GB.UTF-8
+
+# # Set the timezone to London
+# ENV TZ=Europe/London
+# RUN ln -fs /usr/share/zoneinfo/Europe/London /etc/localtime && dpkg-reconfigure -f noninteractive tzdata
+
+# # Install locales and timezone data
+# RUN apt-get update \
+#     && apt-get install -y --no-install-recommends locales tzdata \
+#     && rm -rf /var/lib/apt/lists/* \
+#     && locale-gen en_GB.UTF-8
+
+# # Set environment variables for locale and timezone
+# ENV LANG en_GB.UTF-8
+# ENV LANGUAGE en_GB:en
+# ENV LC_ALL en_GB.UTF-8
+# ENV TZ=Europe/London
--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -868,16 +868,242 @@ do
 // string result = String.Join(" ", newMessage);
 // Console.WriteLine(result);
 
-string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+// string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
 
-string[] orders = orderStream.Split(',');
+// string[] orders = orderStream.Split(',');
 
-foreach (string order in orders)
-{
-    string message = order;
-    if (order.Length != 4)
-    {
-        message += "\t- Error";
-    }
-    Console.WriteLine(message);
-}
+// foreach (string order in orders)
+// {
+//     string message = order;
+//     if (order.Length != 4)
+//     {
+//         message += "\t- Error";
+//     }
+//     Console.WriteLine(message);
+// }
+
+// using System.Globalization;
+
+// string first = "Hello";
+// string second = "World";
+// string result = string.Format("{0} {1}!", first, second);
+// Console.WriteLine(result);
+// Console.WriteLine("{0} {0} {0}!", first, second);
+
+// decimal price = 123.45m;
+// int discount = 50;
+// //displays as currency
+// // CultureInfo cultureInfo = new CultureInfo("en-GB");
+// // Thread.CurrentThread.CurrentCulture = cultureInfo;
+// // Thread.CurrentThread.CurrentUICulture = cultureInfo;
+// Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+// decimal measurement = 123456.78912m;
+// Console.WriteLine($"Measurement: {measurement:N} units");
+
+// decimal tax = .36785m;
+// Console.WriteLine($"Tax rate: {tax:P2}");
+
+// decimal price = 67.55m;
+// decimal salePrice = 59.99m;
+
+// string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+
+// yourDiscount += $"A discount of {((price - salePrice)/price):P2}!"; //inserted
+// Console.WriteLine(yourDiscount);
+
+// int invoiceNumber = 1201;
+// decimal productShares = 25.4568m;
+// decimal subtotal = 2750.00m;
+// decimal taxPercentage = .15825m;
+// decimal total = 3185.19m;
+
+// Console.WriteLine($"Invoice Number: {invoiceNumber}");
+// Console.WriteLine($"   Shares: {productShares:N3} Product");
+// Console.WriteLine($"     Sub Total: {subtotal:C}");
+// Console.WriteLine($"           Tax: {taxPercentage:P2}");
+// Console.WriteLine($"     Total Billed: {total:C}");
+
+// string input = "Pad this";
+//  Console.WriteLine(input.PadLeft(12));
+
+//  Console.WriteLine(input.PadRight(12));
+
+//  Console.WriteLine(input.PadLeft(12, '-'));
+//  Console.WriteLine(input.PadRight(12, '-'));
+
+// string paymentId = "769C";
+//  string payeeName = "Mr. Stephen Ortega";
+//  string paymentAmount = "$5,000.00";
+
+//  var formattedLine = paymentId.PadRight(6);
+//  formattedLine += payeeName.PadRight(24);
+//  formattedLine += paymentAmount.PadLeft(10);
+
+// Console.WriteLine("1234567890123456789012345678901234567890");
+//  Console.WriteLine(formattedLine);
+
+// string customerName = "Ms. Barros";
+
+// string currentProduct = "Magic Yield";
+// int currentShares = 2975000;
+// decimal currentReturn = 0.1275m;
+// decimal currentProfit = 55000000.0m;
+
+// string newProduct = "Glorious Future";
+// decimal newReturn = 0.13125m;
+// decimal newProfit = 63000000.0m;
+
+// Console.WriteLine($"Dear {customerName},");
+// Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+// Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P2}.\n");
+// Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}. Given your current volume, your potential profit would be {newProfit:C}.\n");
+// Console.WriteLine("Here's a quick comparison:\n");
+
+// string comparisonMessage = "";
+
+// comparisonMessage = currentProduct.PadRight(20);
+// comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+// comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+// comparisonMessage += "\n";
+// comparisonMessage += newProduct.PadRight(20);
+// comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+// comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+// Console.WriteLine(comparisonMessage);
+
+// string message = "Find what is (inside the parentheses)";
+
+// int openingPosition = message.IndexOf('(');
+// int closingPosition = message.IndexOf(')');
+
+// Console.WriteLine(openingPosition);
+// Console.WriteLine(closingPosition);
+
+// openingPosition += 1;
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
+
+// string message = "What is the value <span>between the tags</span>?";
+
+// int openingPosition = message.IndexOf("<span>");
+// int closingPosition = message.IndexOf("</span>");
+
+// openingPosition += 6; //avoid magic numbers
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
+
+// string message = "What is the value <span>between the tags</span>?";
+
+// const string openSpan = "<span>";
+// const string closeSpan = "</span>";
+
+// int openingPosition = message.IndexOf(openSpan);
+// int closingPosition = message.IndexOf(closeSpan);
+
+// openingPosition += openSpan.Length;
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
+
+// string message = "(What if) there are (more than) one (set of parentheses)?";
+// while (true)
+// {
+//     int openingPosition = message.IndexOf('(');
+//     if (openingPosition == -1) break;
+
+//     openingPosition += 1;
+//     int closingPosition = message.IndexOf(')');
+//     int length = closingPosition - openingPosition;
+//     Console.WriteLine(message.Substring(openingPosition, length));
+
+//     // Note the overload of the Substring to return only the remaining 
+//     // unprocessed message:
+//     message = message.Substring(closingPosition + 1);
+// }
+
+// string message = "Help (find) the {opening symbols}";
+// Console.WriteLine($"Searching THIS Message: {message}");
+// char[] openSymbols = { '[', '{', '(' };
+// int startPosition = 6;
+// int openingPosition = message.IndexOfAny(openSymbols);
+// Console.WriteLine($"Found WITHOUT using startPosition: {message.Substring(openingPosition)}");
+
+// openingPosition = message.IndexOfAny(openSymbols, startPosition);
+// Console.WriteLine($"Found WITH using startPosition: {message.Substring(openingPosition)}");
+
+// string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+
+// // The IndexOfAny() helper method requires a char array of characters. 
+// // You want to look for:
+
+// char[] openSymbols = { '[', '{', '(' };
+
+// // You'll use a slightly different technique for iterating through 
+// // the characters in the string. This time, use the closing 
+// // position of the previous iteration as the starting index for the 
+// //next open symbol. So, you need to initialize the closingPosition 
+// // variable to zero:
+
+// int closingPosition = 0;
+
+// while (true)
+// {
+//     int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+
+//     if (openingPosition == -1) break;
+
+//     string currentSymbol = message.Substring(openingPosition, 1);
+
+//     // Now  find the matching closing symbol
+//     char matchingSymbol = ' ';
+
+//     switch (currentSymbol)
+//     {
+//         case "[":
+//             matchingSymbol = ']';
+//             break;
+//         case "{":
+//             matchingSymbol = '}';
+//             break;
+//         case "(":
+//             matchingSymbol = ')';
+//             break;
+//     }
+
+//     // To find the closingPosition, use an overload of the IndexOf method to specify 
+//     // that the search for the matchingSymbol should start at the openingPosition in the string. 
+
+//     openingPosition += 1;
+//     closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+//     // Finally, use the techniques you've already learned to display the sub-string:
+
+//     int length = closingPosition - openingPosition;
+//     Console.WriteLine(message.Substring(openingPosition, length));
+// }
+
+// string data = "12345John Smith          5000  3  ";
+// string updatedData = data.Remove(5, 20);
+// Console.WriteLine(updatedData);
+
+// string message = "This--is--ex-amp-le--da-ta";
+// message = message.Replace("--", " ");
+// message = message.Replace("-", "");
+// Console.WriteLine(message);
+
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+int startTagLength = "<span>".Length;
+int startPosition = input.IndexOf("<span>") + startTagLength;
+int stopPosition = input.IndexOf("</span>");
+quantity = "Quantity: " + input.Substring(startPosition, stopPosition - startPosition);
+
+output = input.Replace("<div>", "").Replace("</div>", "");
+output = "Output: " + output.Replace("&trade", "&reg");
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
