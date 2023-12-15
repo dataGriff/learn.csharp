a8df2dc611eb5f003ee1f395d1b0978492b6ea7d
griff182uk <griff182uk@yahoo.co.uk>
2023-12-15 16:54:33 +0000

    done methods that take params

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1275,22 +1275,153 @@ else ipAddress is invalid
 //     validRange = true;
 // }
 
-Random random = new Random();
-int luck = random.Next(100);
+// Random random = new Random();
+// int luck = random.Next(100);
 
-string[] text = {"You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to"};
-string[] good = {"look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!"};
-string[] bad = {"fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life."};
-string[] neutral = {"appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature."};
+// string[] text = {"You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to"};
+// string[] good = {"look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!"};
+// string[] bad = {"fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life."};
+// string[] neutral = {"appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature."};
 
-TellFortune();
+// TellFortune();
 
-void TellFortune() 
+// void TellFortune() 
+// {
+//   Console.WriteLine("A fortune teller whispers the following words:");
+//     string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
+//     for (int i = 0; i < 4; i++) 
+//     {
+//         Console.Write($"{text[i]} {fortune[i]} ");
+//     }
+// }
+// int[] schedule = {800, 1200, 1600, 2000};
+
+// DisplayAdjustedTimes(schedule, 6, -6);
+
+// void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
+// {
+//   int diff = 0;
+//   if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+//   {
+//     Console.WriteLine("Invalid GMT");
+//   }
+//   else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+//   {
+//     diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+//   }
+//   else
+//   {
+//     diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+//   }
+
+//   for (int i = 0; i < times.Length; i++)
+//   {
+//     int newTime = ((times[i] + diff)) % 2400;
+//     Console.WriteLine($"{times[i]} -> {newTime}");
+//   }
+
+// }
+
+// double pi = 3.14159;
+// PrintCircleInfo(12);
+// PrintCircleInfo(24);
+
+// void PrintCircleInfo(int radius) 
+// {
+//     Console.WriteLine($"Circle with radius {radius}");
+//     PrintCircleArea(radius);
+//     PrintCircleCircumference(radius);
+// }
+
+// void PrintCircleArea(int radius)
+// {
+//     double area = pi * (radius * radius);
+//     Console.WriteLine($"Area = {area}");
+// }
+
+// void PrintCircleCircumference(int radius)
+// {
+//     double circumference = 2 * pi * radius;
+//     Console.WriteLine($"Circumference = {circumference}");
+// }
+
+// string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+// string[] rsvps = new string[10];
+// int count = 0;
+
+// RSVP("Rebecca");
+// RSVP("Nadia", 2, "Nuts");
+// RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+// RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+// RSVP("Noor", 4, inviteOnly: false);
+// RSVP("Jonte", 2, "Stone fruit", false);
+// ShowRSVPs();
+
+// void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+// {
+//   if (inviteOnly)
+//   {
+//     bool found = false;
+//     foreach (string guest in guestList)
+//     {
+//       if (guest.Equals(name))
+//       {
+//         found = true;
+//         break;
+//       }
+//     }
+//     if (!found)
+//     {
+//       Console.WriteLine($"Sorry, {name} is not on the guest list");
+//       return;
+//     }
+//   }
+
+//   rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+//   count++;
+// }
+
+// void ShowRSVPs()
+// {
+//   Console.WriteLine("\nTotal RSVPs:");
+//   for (int i = 0; i < count; i++)
+//   {
+//     Console.WriteLine(rsvps[i]);
+//   }
+// }
+
+string[,] corporate = 
 {
-  Console.WriteLine("A fortune teller whispers the following words:");
-    string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
-    for (int i = 0; i < 4; i++) 
-    {
-        Console.Write($"{text[i]} {fortune[i]} ");
-    }
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+
+string internalDomain = "contoso.com";
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    GetEmail(firstName: corporate[i, 0], lastName: corporate[i, 1], domain: internalDomain);
 }
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    GetEmail(firstName: external[i, 0], lastName: external[i, 1], domain: externalDomain);
+}
+
+void GetEmail(string firstName, string lastName, string domain) 
+{
+    
+    string firstTrim = firstName.Substring(0, 2);
+    string email = $"{firstTrim}{lastName}@{domain}".ToLower();
+    Console.WriteLine(email);
+}
+
